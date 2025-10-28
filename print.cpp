0ced7f1854912f58394fb9715f808811897cf5ee
soraawistaria <amaliasoraya2109@gmail.com>
2025-10-28 20:23:04 +0800

    Add files via upload

---
+++ print.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // ini gunanya memanggil fungsi input-output (cin,cout)
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Aku akan membantu Pak Dengklek 1000 masalah";
+}
