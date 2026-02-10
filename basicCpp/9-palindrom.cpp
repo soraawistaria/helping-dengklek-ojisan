864293247271c25f17c41b9d8c898f5f9e57ea98
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-02-10 08:43:18 +0800

    palindrom

--- basicCpp/9-palindrom.cpp
+++ basicCpp/9-palindrom.cpp
@@ -6,7 +6,7 @@ bool palindrom(string s) {
     if (s.length() <= 1) {
         return true;
     } else if (s[0] == s[s.length() - 1]) {
-        return palindrom((s.substr(1, s.length() - 2)));
+        return palindrom(s.substr(1, s.length() - 2));
     } else {
         return false;
     }
