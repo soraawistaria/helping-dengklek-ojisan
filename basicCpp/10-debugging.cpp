04d88b7d0f368ba5b0a23f61b11f5e8b66f79c71
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-02-11 09:28:04 +0800

    debugging💦

---
+++ basicCpp/10-debugging.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    long long jumlah = 0;
+    for (int i = 1; i <= N; i++) {
+        int x;
+        cin >> x;
+        jumlah += x;
+        // cout << "saat i = " << i << ", jumlah sementara = " << jumlah << endl;
+    }
+
+    cout << jumlah << endl;
+}
