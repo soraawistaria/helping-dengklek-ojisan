8ea032e64533a0e2821b860d0076ee4dd0f3daec
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-03-22 21:41:25 +0800

    pola bilangan

---
+++ bab12basic/polabilangan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int count = 0;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j <= i; j++){
+            cout << count % 10;
+            count++;
+        }
+        cout << endl;
+    }
+}
